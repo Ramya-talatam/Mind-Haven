7029c047587bc767907f472d3e104af5a8124f6f
Ramya-talatam <108510824+Ramya-talatam@users.noreply.github.com>
2024-01-18 21:53:57 +0530

    Update journal.js

--- api/static/js/journal.js
+++ api/static/js/journal.js
@@ -2,17 +2,25 @@ function analyzeJournal() {
     var journalText = document.getElementById("journalInput").value;
     var reportElement = document.getElementById("report");
     reportElement.innerHTML = "";
-
+    // const journalApi = 'https://mh-backend-xi.vercel.app/analyze';
+    const journalApi = 'http://127.0.0.1:5000/analyze';
     // Send journal text to server for analysis
-    fetch('/analyze', {
+    fetch(journalApi, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: 'journalText=' + encodeURIComponent(journalText)
     })
-    .then(response => response.json())
+    .then(response => {
+        // console.log('Response:', response);
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        return response.json();
+    })
     .then(data => {
+        // console.log('Data from server:', data);
         var positiveWords = data.positive_words;
         var negativeWords = data.negative_words;
         var focusedWords = data.focused_words;
@@ -22,20 +30,20 @@ function analyzeJournal() {
         positiveWords.forEach(function(wordObj) {
             var wordElement = document.createElement("span");
             wordElement.className = "word positive";
-            wordElement.style.fontSize = (wordObj[1] * 20 + 100) + "%";
+            wordElement.style.fontSize = (wordObj[1] * 200 + 100) + "%";
             wordElement.innerText = wordObj[0] + " ";
             reportElement.appendChild(wordElement);
         });
-
+        reportElement.innerHTML += "<hr>";
         // Display negative words
         negativeWords.forEach(function(wordObj) {
             var wordElement = document.createElement("span");
             wordElement.className = "word negative";
-            wordElement.style.fontSize = (wordObj[1] * -20 + 100) + "%";
+            wordElement.style.fontSize = (wordObj[1] * -200 + 100) + "%";
             wordElement.innerText = wordObj[0] + " ";
             reportElement.appendChild(wordElement);
         });
-
+        reportElement.innerHTML += "<hr>";
         // Display focused words
         focusedWords.forEach(function(word) {
             var wordElement = document.createElement("span");
@@ -43,10 +51,13 @@ function analyzeJournal() {
             wordElement.innerText = word + " ";
             reportElement.appendChild(wordElement);
         });
-
+        reportElement.innerHTML += "<hr>";
         // Display overall polarity
         var polarityElement = document.createElement("p");
         polarityElement.innerText = "Overall Polarity: " + overallPolarity;
         reportElement.appendChild(polarityElement);
+    })
+    .catch(error => {
+        console.error('Error:', error);
     });
 }
