b19957030237125ecfdda3416e662dc2c4e9052c
Ramya-talatam <108510824+Ramya-talatam@users.noreply.github.com>
2024-01-19 01:35:05 +0530

    Update home.py

--- api/home.py
+++ api/home.py
@@ -1,4 +1,4 @@
-nfrom flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
