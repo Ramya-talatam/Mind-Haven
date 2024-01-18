8aaf362488d61a970e1cfc406f41709ac4914038
Ramya-talatam <108510824+Ramya-talatam@users.noreply.github.com>
2024-01-18 21:53:25 +0530

    Update settings.js

--- api/static/js/settings.js
+++ api/static/js/settings.js
@@ -10,4 +10,19 @@ document.getElementById('settings-icon').addEventListener('click', function() {
       document.getElementById('theme-select').addEventListener('change', function() {
           var theme = this.value;
           document.getElementById('theme-link').href = theme;
+          localStorage.setItem('selectedTheme', theme);
       });
+
+$(document).ready(function() {
+    var theme=localStorage.getItem('selectedTheme');
+    if(theme){
+    document.getElementById('theme-link').href =theme;
+    document.getElementById('theme-select').value=theme;
+    }
+    else{
+      document.getElementById('theme-link').href ="{{ url_for('static', filename='css/winterfrost.css') }}";
+    }
+});
+window.addEventListener('load', function () {
+    document.body.classList.add('loaded');
+});
